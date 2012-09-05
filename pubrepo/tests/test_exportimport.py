f1d72b85506de3db7bfb6b78b5c1a8276c73cc90
Michael Mulich <michael.mulich@gmail.com>
2012-09-05 09:04:23 -0400

    Split the first test into three and refactor.

--- pubrepo/tests/test_exportimport.py
+++ pubrepo/tests/test_exportimport.py
@@ -54,29 +54,64 @@ class ExportTest(unittest.TestCase):
         DBSession.remove()
         testing.tearDown()
 
+    def _get_module(self, id=1):
+        """Grab the specified module out of the DB"""
+        from ..models import Module
+        module = DBSession.query(Module).filter_by(id=1).first()
+        return module
+
+    def _write_export_to_filesystem(self, stream, filename='export.zip'):
+        """Write the export stream to the filesystem."""
+        zipfile_path = os.path.join(self.export_path, filename)
+        with open(zipfile_path, 'wb') as file:
+            file.write(stream.getvalue())
+        return zipfile_path
+
+    def test_export_is_a_zip_file(self):
+        # Export a specific module and ensure the file is a valid zip
+        # file.
+        module = self._get_module()
+        # By default, the export_module function outputs a bytestream,
+        # so we need to push that stream on to the filesystem to read
+        # from it more than once.
+        from ..exportimport import export_module
+        export_stream = export_module(module)
+        zipfile_path = self._write_export_to_filesystem(export_stream)
+
+        # Check that the file is a valid .zip.
+        self.assertTrue(zipfile.is_zipfile(zipfile_path))
+
     def test_exporting_a_module_w_resources(self):
-        # Export a specific module and its resource(s) to the
-        # filesystem.
+        # Export a specific module and its resource(s).
         from ..models import Module
         module = DBSession.query(Module).filter_by(id=1).first()
         # By default, the export_module function outputs a bytestream,
         # so we need to push that stream on to the filesystem to read
         # from it more than once.
-        export_filename = 'module_export.zip'
-        zipfile_path = os.path.join(self.export_path, export_filename)
-
         from ..exportimport import export_module
-        with open(zipfile_path, 'wb') as file:
-            file.write(export_module(module).getvalue())
+        export_stream = export_module(module)
+        zipfile_path = self._write_export_to_filesystem(export_stream)
 
-        # Check that the file is a valid .zip.
-        self.assertTrue(zipfile.is_zipfile(zipfile_path))
-        # Expand the contents of the zip file.
+       # Expand the contents of the zip file.
         with zipfile.ZipFile(zipfile_path) as file:
             file_list = file.namelist()
-            file.extractall(self.export_path)
         self.assertIn("{0}.html".format(module.id), file_list)
         self.assertIn("resources/image.png", file_list)
+
+    def test_exported_module_contents_r_accurate(self):
+        # Export a specific module and its resource(s) to the
+        # filesystem for detailed inspection.
+        from ..models import Module
+        module = DBSession.query(Module).filter_by(id=1).first()
+        # By default, the export_module function outputs a bytestream,
+        # so we need to push that stream on to the filesystem to read
+        # from it more than once.
+        from ..exportimport import export_module
+        export_stream = export_module(module)
+        zipfile_path = self._write_export_to_filesystem(export_stream)
+
+        with zipfile.ZipFile(zipfile_path) as file:
+            file.extractall(self.export_path)
         # Check resource(s) is present and matches the information we
         # have stored.
         resources = module.resources
